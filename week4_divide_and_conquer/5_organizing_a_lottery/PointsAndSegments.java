ff2bd9656d30453cd5672f3f7ed61a2868329584
Sathyaa <st630499@dal.ca>
2020-06-17 23:41:33 -0300

    number of segment that contain the given point

--- week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
+++ week4_divide_and_conquer/5_organizing_a_lottery/PointsAndSegments.java
@@ -1,66 +1,67 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PointsAndSegments {
 
+
+
+/*  
+Task: You are given a set of points on a line and a set of segments on a line. The goal is to compute, for
+each point, the number of segments that contain this point.   
+
+Input:
+3 2
+0 5
+-3 2
+7 10
+1 6
+
+Output:
+2 0
+
+Here, we have two segments and three points. The first point lies only in the first segment while the
+remaining two points are outside of all the given segments.
+*/
+public class PointsAndSegments {
+    // 
+	
+	// 2nLog(n) + 2Log(n)  =   O(p+nLog(n))
     public int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
         int[] cnt = new int[points.length];
         int total = starts.length;
-        Arrays.sort(starts);
-        Arrays.sort(ends);
+        Arrays.sort(starts);			// O(nLog(n))
+        Arrays.sort(ends);				// O(nLog(n))
         int i =0;
-        for ( int p : points) {
+        for ( int p : points) {			// O(p)
         	
         	cnt[i++] = countSegements(starts, ends, p , total);
-        	System.out.println(cnt[i-1]);
+        	//System.out.println(cnt[i-1]);
+        	
         }
 
 
-// -3 0  7  
-//  2 5 10
-        // 3 - 1 - 0   =2 -- 1 
-        // 3 - 1 - 2   = 0 -- 6
-        
-        
         return cnt;
     }
-
+    
+    // Number of segments which contain the point will be total - (no of start points which is greater than P) - ( No of end points which is lesser than P)
     private int countSegements(int[] starts, int[] ends, int p, int total) {
     	int leftCount=0, rightCount=0;
-    	leftCount= total- (findLeftIndex(starts, p)+1);
-    	rightCount = (findRightIndex(ends,p)+1);
+    	int left = 0;
+ 	    int right = starts.length-1;
+ 	    int mid = left + (right - left)/2;
+    	leftCount= total- (findLeftIdxByBinarySearch(starts,left, left + (right - left)/2, right, p)  + 1);   	//Log(n)
+    	rightCount = (findRightIdxByBinarySearch(ends,left, mid, right, p) + 1);								//Log(n)
     	
     	return total-leftCount-rightCount;
     	
-		//return 0;
 	}
    
-   private int findLeftIndex(int[] a, int num) {
-	   int left = 0;
-	   int right = a.length-1;
-	   int mid = left + (right - left)/2;
-	   int idx = -1;
-	   idx = findLeftIdxByBinarySearch(a,left, mid, right, num);
-	   System.out.println("num: "+num + " idx "+idx);
-	   
-	   return idx;
-   }
-
-   private int findRightIndex(int[] a, int num) {
-	   int left = 0;
-	   int right = a.length-1;
-	   int mid = left + (right - left)/2;
-	   int idx = -1;
-	   idx = findRightIdxByBinarySearch(a,left, mid, right, num);
-	   System.out.println("num: "+num + " idx "+idx);
-	   
-	   return idx;
-   }
-   
+    // find the starting index of the point using binary search so that the index to the right is greater than num
     private int findLeftIdxByBinarySearch(int[] a, int left, int mid, int right, int num) {
-    	if(num > a[right] ) return right;
+    	if(num >= a[right] ) return right;
     	else if(num< a[left]) return -1;
     	else if(mid == right ) return -1;
+    	
+    	// checks if next index is greater than num and current index is less than or equal to num
     	else if(mid < right && a[mid] <= num && a[mid+1] > num)
     		return mid;
     	else if(a[mid] > num)
@@ -70,16 +71,20 @@ public class PointsAndSegments {
     	else 
     		return -1;
 	
-}
+    }
+    
+    // find the starting index of the point using binary search so that the index to the left is lesser than num
     private int findRightIdxByBinarySearch(int[] a, int left, int mid, int right, int num) {
     	if(num > a[right] ) return right;
     	else if(num< a[left]) return -1;
     	else if(mid == right ) return -1;
+    	
+    	// checks if next index is greater than or equal to num and current index is less than num
     	else if(mid < right && a[mid] < num && a[mid+1] >= num)
     		return mid;
     	else if(a[mid] >= num)
     		return findRightIdxByBinarySearch(a, left, left+(mid-left)/2 , mid, num);
-    	else if(a[mid] <= num)
+    	else if(a[mid] < num)
     		return findRightIdxByBinarySearch(a, mid, mid+(right-mid)/2, right, num);
     	else 
     		return -1;
@@ -114,14 +119,16 @@ public class PointsAndSegments {
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
         }
-        for (int x : PointsAndSegments.naiveCountSegments(starts, ends, points)) {
-            System.out.print(x + " ");
-        }
+		/*
+		 * for (int x : PointsAndSegments.naiveCountSegments(starts, ends, points)) {
+		 * System.out.print(x + " "); }
+		 */
         //use fastCountSegments
         int[] cnt = obj.fastCountSegments(starts, ends, points);
         for (int x : cnt) {
             System.out.print(x + " ");
         }
+        scanner.close();
     }
 }
 
